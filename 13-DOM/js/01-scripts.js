3b402b7b7ba0ec2ab5d0bccf01678a85fef30956
Victor <victor1203rmmtz@gmail.com>
2024-08-20 23:06:28 -0600

    20/08/24

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,25 @@
+//ACEDER A ELEMENTOS DEL HTML
+let elemeto;
+
+elemeto = document;
+elemeto = document.all;
+elemeto = document.head;
+elemeto = document.body;
+elemeto = document.domain;
+elemeto = document.forms;
+elemeto = document.forms[0];
+elemeto = document.forms[0].id;
+elemeto = document.forms[0].method;
+elemeto = document.forms[0].classList;
+elemeto = document.forms[0].action;
+
+elemeto = document.links;
+elemeto = document.links[4];
+elemeto = document.links[4].classList;
+elemeto = document.links[4].className;
+
+elemeto = document.images;
+
+elemeto = document.scripts;
+
+console.log(elemeto);
---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,10 @@
+//SELECCIONAR ELEMENTOS POR SU CLASE...
+
+const header = document.getElementsByClassName("header");
+
+console.log(header);
+
+
+//SI LAS CLASES EXISTEN MAS DE 1 VEZ
+const contenedores = document.getElementsByClassName('contenedor');
+console.log(contenedores);
