2133389dd3b3c9504bd01151700f42e60fd107a6
Victor <victor1203rmmtz@gmail.com>
2024-08-27 23:59:56 -0600

    27/08/24

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const datosUser = {
+    email: "",
+    asunto: "",
+    mensaje: "",
+  };
+
   //SELECCIONAMOS LOS COMPONENTES DE LA INTERFAZ
   const inputEmial = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
@@ -16,12 +22,25 @@ document.addEventListener("DOMContentLoaded", function () {
         `El campo ${e.target.id} es obligatorio`,
         e.target.parentElement
       );
-    } else {
-      console.log("Hay algo");
+      return;
     }
+    if (e.target.id === "email" && !validarCorreo(e.target.value)) {
+      mostrarAlerta("El email no es valido", e.target.parentElement);
+      return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
+
+    //ASIGNAR LOS VALORES
+    datosUser[e.target.name] = e.target.value.trim().toLowerCase();
+
+    //COMPROBAR EL EMAIL
+    comprobarEmail();
   }
 
   function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
+
     //GENERAR ALERTA EN HTML
     const error = document.createElement("P");
     error.textContent = mensaje;
@@ -29,5 +48,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     //INYECTAR EL ERROR AL FORMULARIO
     referencia.appendChild(error);
+  }
+
+  function limpiarAlerta(referencia) {
+    //COMPROBAR SI YA EXISTE UNA ALERTA
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
+    }
+  }
+
+  function validarCorreo(email) {
+    //EL regax ES UNA EXPRESION REGULAR
+    const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const resultado = regex.test(email);
+    return resultado;
+  }
+  function comprobarEmail() {
+    console.log(Object.values(email).includes(""));
   }
 });
